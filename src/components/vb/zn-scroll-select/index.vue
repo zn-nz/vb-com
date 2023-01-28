720e4e4f965872c197943e01187a31bb73ede754
失控的括约肌 <zn163email@163.com>
2023-01-28 09:21:50 +0800

    u

--- src/components/vb/zn-scroll-select/index.vue
+++ src/components/vb/zn-scroll-select/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<el-select :model-value="value" popper-class="scroll-select">
+	<el-select :model-value="value" :popper-class="popperClass" @focus="initScroll">
 		<slot></slot>
 	</el-select>
 </template>
@@ -15,23 +15,23 @@ const props = defineProps({
 	}
 });
 const emit = defineEmits(["loadMore"]);
+const popperClass = `cl-${Date.now()}-${Math.random().toString().replace('.', '')}`;
 let dom;
 onMounted(() => {
 	initScroll();
 });
 onUnmounted(() => {
-	dom.removeEventListener("scroll", scrollEvent);
+	dom?.removeEventListener("scroll", scrollEvent);
 });
 function initScroll() {
-	dom = document.querySelector(".scroll-select .el-scrollbar__wrap");
-	dom.addEventListener("scroll", scrollEvent);
+	setTimeout(() => {
+		dom = document.querySelector(`.${popperClass} .el-scrollbar__wrap`);
+		dom?.addEventListener("scroll", scrollEvent);
+	}, 0);
 }
 // eslint-disable-next-line object-curly-spacing
 function scrollEvent({ target: { offsetHeight, scrollHeight, scrollTop } }) {
-	if (
-		offsetHeight + scrollTop > scrollHeight - props.diffBottom &&
-		!props.loading
-	) {
+	if (offsetHeight + scrollTop > scrollHeight - props.diffBottom && !props.loading) {
 		emit("loadMore");
 	}
 }
