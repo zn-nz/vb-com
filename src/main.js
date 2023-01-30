f14d5d5d1acad9f89e99dd670e781798c27e7571
失控的括约肌 <zn163email@163.com>
2023-01-30 11:17:40 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import VbCom from "./components/vb/index";
-// import VbCom from "vb-com";
+// import VbCom from "./components/vb/index";
+import VbCom from "vb-com";
+import "element-plus/dist/index.css";
 
 const app = createApp(App);
 app.use(VbCom);
